7284f11b246a81694b2f64b00fb8358020cdd8c9
David Faith <faithodesola@gmail.com>
2022-02-05 03:28:29 +0000

    Design the ScriptInfo Page

--- components/ScriptCard.tsx
+++ components/ScriptCard.tsx
@@ -1,4 +1,5 @@
 import { Avatar, makeStyles, Theme } from "@material-ui/core";
+import Link from "next/link";
 
 const useStyles = makeStyles((theme: Theme) => ({
   avatar: {
@@ -12,36 +13,38 @@ const useStyles = makeStyles((theme: Theme) => ({
 const ScriptCard = () => {
   const classes = useStyles();
   return (
-    <div className="flex flex-col rounded-t-md rounded-b-md shadow-lg">
-      <img
-        className="h-28 object-cover w-full rounded-t-md"
-        src="/images/leaves.jpg"
-      />
-      <div className="w-full h-2  bg-neutral-800"></div>
-      <div className="bg-[#36395A] p-3 rounded-b-md shadow-gray-700">
-        <h1 className="font-bold text-sm text-neutral-100">
-          The Beginning of a New Thing
-        </h1>
-        <h1 className="text-sm font-light my-2 text-neutral-200 leading-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna....
-        </h1>
-        <div className="w-full flex justify-between">
-          <div className="flex items-center">
-            <Avatar
-              alt="Remy Sharp"
-              className={classes.avatar}
-              src="/images/profile.jpg"
-            />
-            <h1 className="text-white font-medium text-sm">david0</h1>
-          </div>
-          <div>
-            <h1 className="text-white text-xs text-right">12/12/2021</h1>
-            <h1 className="text-white text-sm font-bold">Short Films </h1>
+    <Link href="/scripts/12345" passHref>
+      <div className="flex flex-col rounded-t-md rounded-b-md shadow-lg hover:cursor-pointer hover:shadow-2xl">
+        <img
+          className="h-28 object-cover w-full rounded-t-md"
+          src="/images/leaves.jpg"
+        />
+        <div className="w-full h-2  bg-neutral-800"></div>
+        <div className="bg-[#36395A] p-3 rounded-b-md shadow-gray-700">
+          <h1 className="font-bold text-sm text-neutral-100">
+            The Beginning of a New Thing
+          </h1>
+          <h1 className="text-sm font-light my-2 text-neutral-200 leading-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna....
+          </h1>
+          <div className="w-full flex justify-between">
+            <div className="flex items-center">
+              <Avatar
+                alt="Remy Sharp"
+                className={classes.avatar}
+                src="/images/profile.jpg"
+              />
+              <h1 className="text-white font-medium text-sm">david0</h1>
+            </div>
+            <div>
+              <h1 className="text-white text-xs text-right">12/12/2021</h1>
+              <h1 className="text-white text-sm font-bold">Short Films </h1>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
---
+++ pages/scripts/[id].tsx
@@ -0,0 +1,95 @@
+import Head from "next/head";
+import CategoryList from "../../components/CategoryList";
+import { useRouter } from "next/router";
+import Layout from "../../components/Layout";
+import { NextPage } from "next";
+import ScriptCard from "../../components/ScriptCard";
+import { Avatar, makeStyles, Theme } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  avatar: {
+    // marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 80,
+    height: 80,
+  },
+}));
+
+const ScriptInfo: NextPage = () => {
+  const classes = useStyles();
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <>
+      <Head>
+        <title> Script | ScriptRoom</title>
+      </Head>
+      <Layout>
+        <CategoryList />
+        <div className="flex p-3 flex-col md:flex-row">
+          <div className="flex-3 bg-neutral-50 p-3 md:p-8 m-0 md:m-5  rounded-md">
+            <img
+              className="h-32 object-cover w-full rounded-t-md"
+              src="/images/leaves.jpg"
+            />
+            <div className="flex justify-between w-full items-center my-3">
+              <h1 className="font-bold text-2xl text-[#36395A]">
+                The Beginning of a New Thing
+              </h1>
+              <button className="px-6 py-2 rounded-md bg-[#36395A]  hover:bg-slate-800 text-white text-sm font-normal uppercase focus:outline-none">
+                View
+              </button>
+            </div>
+            <div>
+              <h1 className="font-medium">Description</h1>
+              <p className="text-gray-600 text-sm my-1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                voluptas sit aspernatur aut odit aut fugit, sed quia
+                consequuntur magni dolores eos qui ratione voluptatem sequi
+                nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+                sit amet, consectetur, adipisci velit, sed quia non numquam eius
+                modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+                voluptatem. Ut enim ad minima veniam, quis nostrum
+                exercitationem ullam corporis suscipit laboriosam, nisi ut
+                aliquid ex ea commodi consequatur? Quis autem vel eum iure
+                reprehenderit qui in ea voluptate velit esse quam nihil
+                molestiae consequatur, vel illum qui dolorem eum fugiat quo
+                voluptas nulla pariatur?
+              </p>
+            </div>
+          </div>
+          <div className="flex-2 bg-neutral-50 my-5 rounded-md p-5 h-fit">
+            <h1 className="font-semibold text-[#36395A]">About the Author</h1>
+            <div className="flex flex-col items-center my-2">
+              <Avatar
+                alt="Remy Sharp"
+                className={classes.avatar}
+                src="/images/profile.jpg"
+              />
+              <h1 className="font-medium">david0</h1>
+            </div>
+            <p className="text-gray-600 text-sm my-1">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default ScriptInfo;
