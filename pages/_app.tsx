c52bb2596a2f45adc32555b8b91e379c7ebd69b4
David Faith <faithodesola@gmail.com>
2022-04-13 02:47:10 +0100

    Add Nprogress to routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,14 @@ import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import { RecoilRoot } from "recoil";
 import Auth from "../hoc/auth";
+import "../styles/nprogress.css";
+import nProgress from "nprogress";
+import { Router } from "next/router";
+
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
