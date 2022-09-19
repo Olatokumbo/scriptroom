13a05a10e63d79b630c026c222bd92eb5fe5eec8
David Faith <faithodesola@gmail.com>
2022-09-19 16:37:14 +0100

    Add _document file

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <meta name="theme-color" content="#36395A" />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
